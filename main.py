d486e2b745fbd7e09212b68d5d3072f1884bf4c8
Илья Горбатенко <ilia.sb.sua@yandex.ru>
2025-01-04 15:20:55 +0300

    Удален график арт

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name, char_class) -> str:
     if char_class == 'warrior':
@@ -97,7 +99,8 @@ def choice_char_class() -> str:
     return str(char_class)
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name = input('...назови себя: ')
@@ -107,6 +110,3 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
